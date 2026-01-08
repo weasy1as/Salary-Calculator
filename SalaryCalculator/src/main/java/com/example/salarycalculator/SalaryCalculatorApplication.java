44d8fa98ffb15d2c4c50f6aa9c459a432c2dcc17
weasy1as <habdu21@student.sdu.dk>
2026-01-08 16:49:56 +0100

    first commit

---
+++ SalaryCalculator/src/main/java/com/example/salarycalculator/SalaryCalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.example.salarycalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalaryCalculatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SalaryCalculatorApplication.class, args);
+    }
+
+}
---
+++ SalaryCalculator/src/test/java/com/example/salarycalculator/SalaryCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.salarycalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalaryCalculatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
